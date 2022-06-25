327ba20dad0609a4d4d6c6b984e56011a544aceb
Shay Forde <shayforde@gmail.com>
2022-06-25 17:14:22 +0100

    first commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,21 @@
+<script>
+    
+</script>
+
+
+<div class="about">
+    <h2>About Us</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae qui unde quaerat neque quis aspernatur consectetur ea eius fugit laudantium omnis doloremque ipsa inventore voluptate, ducimus ullam. Pariatur quidem id assumenda obcaecati quas quia labore tenetur numquam porro a sapiente consequatur, incidunt, explicabo culpa quam natus modi dolores quis commodi et, tempora dolorem rerum ullam. Mollitia minus quasi beatae ab.</p>
+    <a href="/guides">View Guides</a>
+    <a href="/">Home</a>
+</div>
+
+<style>
+    .about {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+       
+    }
+
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,24 @@
+<script>
+   /*
+   https://netninja.dev/courses/sveltekit-tutorial/lectures/36982375
+   */
+</script>
+
+
+<div class="index">
+    <h2>Welcome</h2>
+    <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque dignissimos eligendi, odio molestias explicabo nulla, iusto debitis quam fugiat similique dolores libero impedit, tempora maxime! In, dignissimos. Totam, dolores similique. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, tempora recusandae. Ducimus, cum. Nostrum, perspiciatis inventore repellat libero rem ut voluptate dicta porro quos facere eveniet recusandae, ad, sint sed. Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, error aliquid soluta quasi voluptatem, perferendis explicabo esse quia reprehenderit illum maxime sequi in doloribus! Quam adipisci ad debitis animi nesciunt.
+    </div>
+    <a href="/guides">View Guides</a>
+    <a href="/about">About</a>
+  
+
+</div>
+
+<style>
+    .index {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
